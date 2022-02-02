9d51950230fd1b9003913e087ed5d7e09b011318
abdimoha-dev <abdymohammed@gmail.com>
2022-02-02 16:53:35 +0300

    current retiring staff

--- app/api/helper_methods/cadres.py
+++ app/api/helper_methods/cadres.py
@@ -5,7 +5,7 @@ def get_count_by_cadres():
     if "current_staff" in cache:
         current_staff = cache.get("current_staff")
     else:
-        current_staff = ZebraCurrentStaff.objects.only("person_firstname", "demographic_gender","cadre_name","county_name","ward_district_name","field_currentage","field_hire_year")
+        current_staff = ZebraCurrentStaff.objects.only("person_firstname", "demographic_gender","cadre_name","county_name","ward_district_name","field_currentage","field_hire_year", "field_retirement_date")
         cache.set("current_staff",current_staff, timeout=360000)
         
     all_available_cadres = {}
@@ -16,4 +16,22 @@ def get_count_by_cadres():
         else:
             all_available_cadres[staff.cadre_name] = all_available_cadres[staff.cadre_name]+1
     return all_available_cadres
-    
+
+from datetime import date
+def get_count_retiring():
+    todays_date = date.today()
+    current_year = todays_date.year
+    retiring_staff_count = []
+    current_staff = ZebraCurrentStaff.objects.only("field_retirement_date")
+    for staff in current_staff:
+        # print(datetime.datetime.now())
+        if staff.field_retirement_date is not None:
+            retirement_date = staff.field_retirement_date
+            retiriment_year = retirement_date.strftime('%Y')
+        
+            if retiriment_year == current_year:
+                retiring_staff_count.append(retiriment_year)
+    retirement_count = len(retiring_staff_count)            
+    import pdb
+    pdb.set_trace()
+    pass
--- app/api/views.py
+++ app/api/views.py
@@ -8,7 +8,7 @@ from .models import ZebraCurrentStaff
 import pandas as pd
 
 def test(request):
-    current_staff = ZebraCurrentStaff.objects.only("person_firstname", "demographic_gender","cadre_name","county_name","ward_district_name","field_currentage","field_hire_year")
+    current_staff = ZebraCurrentStaff.objects.only("person_firstname", "demographic_gender","cadre_name","county_name","ward_district_name","field_currentage","field_hire_year", "field_retirement_date")
     records = current_staff.to
     # print(current_staff)
     master_data_frame = pd.DataFrame.from_records(list(current_staff))
@@ -17,13 +17,14 @@ def test(request):
     pdb.set_trace()
     # master_data_frame = pd.DataFrame(list(current_staff.values()))
     
-from .helper_methods.cadres import get_count_by_cadres
+from .helper_methods.cadres import get_count_by_cadres,get_count_retiring
 def home(request):
-    all_current_staff = ZebraCurrentStaff.objects.all().count()
+    # all_current_staff = ZebraCurrentStaff.objects.all().count()
     
-    cadre_table = get_count_by_cadres()
+    # cadre_table = get_count_by_cadres()
+    retiring_next_year = get_count_retiring()
     context = {
-       "all_current_staff": "{:,}".format(all_current_staff), 
-       "cadre_table" : cadre_table
+    #    "all_current_staff": "{:,}".format(all_current_staff), 
+    #    "cadre_table" : cadre_table
     }
     return render(request,'dashboard/dash_content.html', context)
