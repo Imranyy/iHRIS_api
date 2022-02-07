471d498bf61bed3905800af21bd1dcc59bbdda1b
abdimoha-dev <abdymohammed@gmail.com>
2022-02-07 11:06:30 +0300

    update urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('cache/clear',views.clear_cache, name='cache-clear'),
-    # path('api/current_staff', views.all_current_staff, name='current_staff'),
-    # path('api/user_accounts',views.user_account, name='user_account')
+    path('current_staff', views.all_current_staff, name='current_staff'),
+    path('user_accounts',views.user_account, name='user_account')
 ]
